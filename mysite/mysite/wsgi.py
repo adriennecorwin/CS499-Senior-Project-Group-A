def0c63b378c2577037b7926b4f1dd773315b8a2
Adrienne Corwin <adriennecorwin@gmail.com>
2019-12-12 21:25:38 -0500

    updated wsgi to work on server

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/ec2-user/CS499-Senior-Project-A/mysite/')
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/ec2-user/eb-virt/lib/python3.6/dist-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
